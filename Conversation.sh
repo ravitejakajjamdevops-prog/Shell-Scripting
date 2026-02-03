277428cc680386ca2c8245e67449fd7f339098f9
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-03 18:54:39 +0530

    convs

---
+++ Conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh::Hi Ramesh, How are you"
+echo "Ramesh:: Hi Suresh, I am good nd how about you"
+echo "Suresh:: i am good Ramesh thank you, Howz your training is going"
+echo "Ramesh:: Doing good Suresh"
