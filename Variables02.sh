b9ee0e3dd3579873569805ae8be128dd12485875
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-03 20:14:31 +0530

    convs

--- Variables02.sh
+++ Variables02.sh
@@ -4,3 +4,5 @@ end_time=$(date +%s)
 echo " Script executed at $start_time"
 sleep 15
 echo "Script end time at $end_time"
+total_time=$(($start_time - $end_time))
+echo " Script total time is $total_time"
