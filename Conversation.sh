85194e118bb6b59fcfd91b7058350c718b17db38
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-03 19:00:38 +0530

    convs

--- Conversation.sh
+++ Conversation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "Suresh::Hi Ramesh, How are you"
-echo "Ramesh:: Hi Suresh, I am good nd how about you"
-echo "Suresh:: i am good Ramesh thank you, Howz your training is going"
-echo "Ramesh:: Doing good Suresh"
+name1=Suresh
+name2=Ramesh
+
+echo "$name1::Hi $name2, How are you"
+echo "$name2:: Hi $name1, I am good nd how about you"
+echo "$name1:: i am good $name2 thank you, Howz your training is going"
+echo "$name2:: Doing good $name1"
