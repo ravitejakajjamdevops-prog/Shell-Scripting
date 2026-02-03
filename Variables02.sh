2acc5d5e78a05eb7922a2a7f632192f917bb6948
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-03 20:11:22 +0530

    convs

--- Variables02.sh
+++ Variables02.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-TimeStamp=$(date)
-echo " Script executed at $TimeStamp"
+start_time=$(date +%s)
+end_time=$(date +%s)
+echo " Script executed at $start_time"
+sleep 15
+echo "Script end time at $end_time"
