b40e6acf23282bccee3ce2f78aa44e2612846fb0
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-04 02:12:26 +0530

    convs

--- Condition.sh
+++ Condition.sh
@@ -2,6 +2,8 @@
 Number1=$1
 if [ $Number1 -lt 30 ];then
     echo "Given number is less than 30 "
+elif [$Number1 -eq 30 ];then
+    echo "Given number is euqls to 30"    
 else
     echo "Given number is grater than 30" 
 fi    
