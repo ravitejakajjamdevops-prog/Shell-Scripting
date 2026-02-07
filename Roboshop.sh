a8fce566504c6b8e214706caf86ada2870663d7b
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-07 21:49:18 +0530

    Seconf

--- Roboshop.sh
+++ Roboshop.sh
@@ -5,7 +5,7 @@ SG_ID=sg-03773a885c8092230
 for instance in $a
 do
     Instance_ID = $(aws ec2 run-instances \
-    --image-id $AMI_ID \
+    --image-id $AMI_Id \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
