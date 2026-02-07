23081bae66579a1cfedba3b1ada7f80cb7df1b54
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-02-07 22:29:47 +0530

    Seconf

--- Roboshop.sh
+++ Roboshop.sh
@@ -6,10 +6,10 @@ for instance in $@
 do
     Instance_ID=$( aws ec2 run-instances \
     --image-id $AMI_Id \
-    --instance-type "t3.micro" \
+    --instance-type t3.micro \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceID' \
+    --query 'Reservations[0].Instances[0].InstanceID' \
     --output text )
     echo "Instance Id is $Instance_ID"
     if [ $instance == "frontend" ];then
